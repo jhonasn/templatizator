ca36a7b371b016e46643a00c48bd272142b49994
jhonasn <jhonasn@gmail.com>
2018-09-22 19:25:56 -0300

    implemented code prepared to compilation

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from tkinter import Tk
 import pygubu
+from pygubu.builder import ttkstdwidgets
 
 from gui.Window import Window
 
 class App(pygubu.TkApplication):
     def start(self):
         builder = pygubu.Builder()
+        # use build.add_from_string to build package
         builder.add_from_file('./gui/gui.ui')
 
         window = builder.get_object('window', self.master)
