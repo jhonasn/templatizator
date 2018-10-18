03bc2cefc28a0dd78d18fc26235f1b6b8e6f7c6e
Jhonas Aparecido Alves Do Nascimento <jhonasnascimento@brq.com>
2018-10-18 18:27:38 -0300

    implemented ui configurable, block delete configurable content and paint template code

--- presentation/configurable_editor.py
+++ presentation/configurable_editor.py
@@ -1,4 +1,5 @@
 '''Handler for editor window'''
+from difflib import SequenceMatcher
 from presentation.window import Window
 from presentation.editor import Editor
 
@@ -7,6 +8,11 @@ from presentation.editor import Editor
 # pylint: disable=too-many-instance-attributes
 class ConfigurableEditor(Editor):
     '''Configurable editor window class handler'''
+    def __init__(self, builder, application, variable_application):
+        super().__init__(builder, application, variable_application)
+        self.original_text = None
+        self.current_text = None
+
     def rebind(self):
         '''Rebind editor events'''
         super().set_props()
@@ -14,13 +20,47 @@ class ConfigurableEditor(Editor):
         self.cancel_button['command'] = self.cancel
         self.save_button['command'] = self.save
         self.combobox.bind('<<ComboboxSelected>>', self.variable_selected)
+        self.editor.bind('<KeyRelease>', self.key_pressed)
 
         # hide file name
         self.filelabel.grid_forget()
         self.filename.grid_forget()
 
-        self.dialog.withdraw()
         self.dialog.protocol('WM_DELETE_WINDOW', self.cancel)
+
+    # pylint: disable=unused-argument
+    def key_pressed(self, event):
+        '''Updates the UI and get separate template lines
+        when key is pressed
+        '''
+        content = self.editor.get('1.0', 'end')[0:-1]
+        sequence = SequenceMatcher(None, self.original_text, content)
+
+        tags = [tag for tag, o1, o2, c1, c2 in sequence.get_opcodes()]
+        reseted = False
+        if 'replace' in tags or 'delete' in tags:
+            cursor = self.editor.index('insert')
+            self.editor.delete('1.0', 'end')
+            self.editor.insert('1.0', self.current_text)
+            self.editor.mark_set('insert', cursor)
+            reseted = True
+            sequence = SequenceMatcher(None, self.original_text,
+                                       self.current_text)
+
+        self.editor.tag_delete('template')
+        template_lines = []
+
+        # pylint: disable=invalid-name, unused-variable
+        for tag, o1, o2, c1, c2 in sequence.get_opcodes():
+            if tag == 'insert':
+                start, end = f'1.0+{c1}c', f'1.0+{c2}c'
+                self.editor.tag_add('template', start, end)
+                self.editor.tag_config('template', background='lightgreen')
+
+                template_lines.append(content[c1:c2])
+
+        if not reseted:
+            self.current_text = content
 
     # argument required to call the event and not used by application
     # pylint: disable=unused-argument
@@ -60,10 +100,10 @@ class ConfigurableEditor(Editor):
         self.node = node
         self.call_back = call_back
 
+        self.current_text = self.original_text = self.application.get(node)
         self.editor.delete('1.0', 'end')
         self.editor.insert(
-            '1.0',
-            self.application.get(node)
+            '1.0', self.original_text
         )
 
         self.combobox['values'] = ['template.name', 'template.path'] + list(
